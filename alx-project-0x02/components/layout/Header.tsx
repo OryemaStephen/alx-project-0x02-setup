cac3dd7cd17da966a12263a8e6f3aff8d2b9f8f4
OryemaStephen <oryemasteve@gmail.com>
2025-07-22 00:41:49 +0300

    Added home and about pages files and updated header to have the routes

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
-const Header:React.FC = () => {
+const Header: React.FC = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="bg-white shadow p-4 flex justify-between items-center">
+      <nav>
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="/">
+              <span className="hover:text-blue-600 cursor-pointer">Home</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <span className="hover:text-blue-600 cursor-pointer">About</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
